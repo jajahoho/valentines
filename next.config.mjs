3c81c2253daa5cf0f0b1c0d10498477ea1fdeef8
jajahoho <jojoqjchen@gmail.com>
2025-02-07 12:59:33 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/valentines.git/' : '',
-  basePath: isProd ? '/valentines.gite' : '',
+  assetPrefix: isProd ? '/valentines/' : '',
+  basePath: isProd ? '/valentines' : '',
   output: 'export'
 };
 export default nextConfig;
